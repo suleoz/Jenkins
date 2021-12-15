5fdc4146f1a4f8419f37dedb20b1bfc1f40340bb
suleoz <suleozdemir107@gmail.com>
2021-12-15 18:15:49 -0500

    First commit

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[]args) throws InterruptedException {
+
+        for (int i = 0; i <5 ; i++) {
+            System.out.println("batch 10 is great");
+            Thread.sleep(1000);
+        }
+    }
+}
